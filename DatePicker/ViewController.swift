5be7e3512908e82b7fe53e4c308bb3c75bd93983
forthenew <forthenew@gmail.com>
2020-11-30 22:46:32 +0900

    Initial Commit

---
+++ DatePicker/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by stam on 2020/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var alarmTime: String!
+
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblSelectTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @objc func updateTime() {
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        
+        if currentTime == alarmTime {
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.white
+        }
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblSelectTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
+    }
+}
+
